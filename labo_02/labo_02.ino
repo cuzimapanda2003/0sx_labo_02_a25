60202bb850903cd2e48351678f05e5193000592f
cuzimapanda2003 <marcblais03@gmail.com>
2025-02-10 16:58:06 -0500

    Update labo_02.ino
    code led + potentionmetre

--- labo_02/labo_02.ino
+++ labo_02/labo_02.ino
@@ -1,17 +1,59 @@
 unsigned long currentTime = 0;
-int patate = 0;
+const int ledPins[] = {8, 9, 10, 11};
+int button = 2;
+int potentiometerPin = A1;           
+int potentiometerValue = 0;   
 
 void setup() {
   Serial.begin(9600);
 
-  // Ton code ici
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT); 
+  }
 
-
-
+  pinMode(button, INPUT_PULLUP);
   Serial.println("Setup completed");
 }
 
+
+
+
 void loop() {
   currentTime = millis();
+  
+  led();
 
 }
+
+void led(){
+
+  potentiometerValue = analogRead(potentiometerPin);
+  Serial.println(potentiometerValue);
+
+if(potentiometerValue < 256){
+    digitalWrite(ledPins[0], HIGH);
+    digitalWrite(ledPins[1], LOW);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+} 
+else if (potentiometerValue < 512) {
+    digitalWrite(ledPins[0], LOW);
+    digitalWrite(ledPins[1], HIGH);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], LOW);
+}
+else if (potentiometerValue < 768) {
+    digitalWrite(ledPins[0], LOW);
+    digitalWrite(ledPins[1], LOW);
+    digitalWrite(ledPins[2], HIGH);
+    digitalWrite(ledPins[3], LOW);
+}
+else{
+    digitalWrite(ledPins[0], LOW);
+    digitalWrite(ledPins[1], LOW);
+    digitalWrite(ledPins[2], LOW);
+    digitalWrite(ledPins[3], HIGH);
+}
+}
+
+
