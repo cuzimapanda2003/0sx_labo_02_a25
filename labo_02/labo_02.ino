37092452fc6ba1af18d9cfa006a1a227f571d9b2
cuzimapanda2003 <marcblais03@gmail.com>
2025-02-14 11:22:50 -0500

    Update labo_02.ino

--- labo_02/labo_02.ino
+++ labo_02/labo_02.ino
@@ -72,28 +72,20 @@ void buttonPress(){
    int pourcentage = (valeur_converti * mult) / max;
    
 
-  // check to see if you just pressed the button
-  // (i.e. the input went from LOW to HIGH), and you've waited long enough
-  // since the last press to ignore any noise:
-
-  // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
-    // reset the debouncing timer
+   
     lastDebounceTime = millis();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    // whatever the reading is at, it's been there for longer than the debounce
-    // delay, so take it as the actual current state:
 
-    // if the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
 
-      // only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
        Serial.print(pourcentage);
         Serial.print("% [");
+
         for(int i = 0; i < max; i++)
         if(i < valeur_converti){
           Serial.print("@");
@@ -101,15 +93,17 @@ void buttonPress(){
         else{
           Serial.print("-");
         }
-     
-      }
       Serial.println("]");
+      }
+     
+       
     }
+   
   }
 
 
 
-  // save the reading. Next time through the loop, it'll be the lastButtonState:
+ 
   lastButtonState = reading;
 }
 
